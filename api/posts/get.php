438c3e017519c5f867a4790160008028766f55e8
brajevicm <milos.brajevic@hotmail.com>
2017-09-11 18:45:51 +0200

    fixed typo

--- api/posts/get.php
+++ api/posts/get.php
@@ -21,7 +21,6 @@ if (isset($_POST['offset']) && isset($_SERVER['HTTP_TOKEN'])) {
     echo getFilteredPostsLimit($offset);
 } elseif (isset($_POST['post_id']) && isset($_SERVER['HTTP_TOKEN'])) {
     $token = $_SERVER['HTTP_TOKEN'];
-    $offset = $_POST['offset'];
     $post_id = $_POST['post_id'];
     echo getPostForUser($token, $post_id);
 } elseif (isset($_POST['post_id'])) {
